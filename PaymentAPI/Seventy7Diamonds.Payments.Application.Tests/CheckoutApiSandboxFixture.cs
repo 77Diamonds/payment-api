d9fc87181326c32aee64879f3d1e80d90d655846
fabianosalles <fabiano.salles@gmail.com>
2025-02-27 07:52:24 -0300

    add testing for expired visa card

---
+++ PaymentAPI/Seventy7Diamonds.Payments.Application.Tests/CheckoutApiSandboxFixture.cs
@@ -0,0 +1,29 @@
+﻿using Checkout;
+
+namespace Seventy7Diamonds.Payments.Application.Tests;
+
+/// <summary>
+/// A fixture to be used on
+/// </summary>
+public class CheckoutApiSandboxFixture : IDisposable
+{
+    public const string ClientID = "ack_usg74gyctmau3lzxmpm4lvuhju";
+    public const string PublicKey = "pk_sbox_g3iid7ew7clcwgyjn63gxhlc4mj";
+    public const string SecretKey = "sk_sbox_vbgt3aucy7xnj7kftnrhxjmhpm2";
+    public const string ProcessingChannelId = "pc_7b4tpe676mue3bao5nvj5ovzem";
+    public const string AuthURI = "https://access.sandbox.checkout.com/connect/token";
+    public readonly ICheckoutApi CheckoutApi;
+
+    public CheckoutApiSandboxFixture()
+    {
+        CheckoutApi = CheckoutSdk.Builder().StaticKeys()
+            .PublicKey(PublicKey)
+            .SecretKey(SecretKey)
+            .Environment(Checkout.Environment.Sandbox)
+            .Build();
+    }
+
+    public void Dispose()
+    {
+    }
+}
